05bcb00bf7e243663f1192d2cc43bc742c8bd4e1
Damien Stengel <3damyscus@gmail.com>
2024-02-15 11:44:00 +0100

    Correct booking entity to not throw error

--- backend/src/main/java/fr/univcotedazur/isadevops/repositories/ActivityRepository.java
+++ backend/src/main/java/fr/univcotedazur/isadevops/repositories/ActivityRepository.java
@@ -3,9 +3,11 @@ package fr.univcotedazur.isadevops.repositories;
 import fr.univcotedazur.isadevops.entities.Activity;
 import fr.univcotedazur.isadevops.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
     Optional<Activity> findActivityByName(String name);
 }
