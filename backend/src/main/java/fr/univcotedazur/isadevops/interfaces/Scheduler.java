86b95d706fca28dc506e17165c87fe122e3f0323
simonbeurel <simonbeurel@gmail.com>
2024-03-18 10:25:48 +0100

    Test pour vérifier le changement de mon compte

--- backend/src/main/java/fr/univcotedazur/isadevops/interfaces/Scheduler.java
+++ backend/src/main/java/fr/univcotedazur/isadevops/interfaces/Scheduler.java
@@ -1,7 +1,7 @@
 package fr.univcotedazur.isadevops.interfaces;
 
 import java.util.Optional;
-
+//Scheduler pour notre deuxième service externe
 public interface Scheduler {
 
     Optional<String> book(String dateBook, String nameActivity, String namePartner);
